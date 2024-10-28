16c6d2b02ea091f2644c430a3e86832a467d02a2
Mahanthesha-9 <h.mahanthesha@in.bosch.com>
2024-10-28 17:10:20 +0530

    Create StringCalculatorFixture.Test.cpp

---
+++ StringCalculatorFixture.Test.cpp
@@ -0,0 +1,28 @@
+#include "stringCalculator.h"
+#include <gtest/gtest.h>
+
+class string_calculator_add :  public ::testing::Test{
+protected:
+    StringCalculator *objUnderTest;
+    void SetUp() override
+    {
+      objUnderTest = new StringCalculator();
+    }
+    void TearDown() 
+    {
+      delete objUnderTest;
+    }
+};
+
+//TEST(TestSuiteName, TestCaseName)
+TEST_F(string_calculator_add,positive_cases){
+    ASSERT_EQ(objUnderTest->Add(""), 0);
+    ASSERT_EQ(objUnderTest->Add("0"), 0);
+    ASSERT_EQ(objUnderTest->Add("1"), 1);
+    ASSERT_EQ(objUnderTest->Add("1,2"), 3);
+    ASSERT_EQ(objUnderTest->Add("//;\n1;2"), 3);
+    ASSERT_EQ(objUnderTest->Add("42,1001,3",45);
+    ASSERT_EQ(objUnderTest->Add("//[***]\n8***2***3"), 13);
+    ASSERT_EQ(objUnderTest->Add("//[*][%]\n4*2%3"), 9);
+    ASSERT_EQ(objUnderTest->Add("//[**][%^]\n4**1%^9"), 14);
+  }
