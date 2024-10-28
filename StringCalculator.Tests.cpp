8ea9a970aa2ccb4eb2c5227a38a600515b59641f
Mahanthesha-9 <h.mahanthesha@in.bosch.com>
2024-10-28 16:20:59 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -85,3 +85,21 @@ TEST(string_calculator_add,when_passed_a_delimiter){
   int actualValue=objUnderTest.Add(input);
   //ASSERT
   ASSERT_EQ(expectedValue,actualValue);
+
+TEST(string_calculator_add,when_passed_negative_numbers_throws_an_exception_listing_invalid_values){
+  //Arrange
+  StringCalculator objUndertest;
+  string input = "1,-2,-4,5";
+  int expectedValue = 6;
+
+  //act
+    ASSERT_THROW({
+      objUndertest.Add(input);
+    }, std::invalid_argument);
+
+    // try {
+    //     calculator.Add("1,-2,-4,5");
+    // } catch (const std::invalid_argument& e) {
+    //     EXPECT_STREQ(e.what(), "Negatives not allowed: -2,-4");
+    // }
+}
